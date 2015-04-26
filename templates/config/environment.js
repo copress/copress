e48e8f47df955aaa6baaa0206c7c3bec3c624153
webs <rainbluz@gmail.com>
2015-04-26 13:31:23 +0300

    migration expressJS 3 to expressJS 4

--- templates/config/environment.js
+++ templates/config/environment.js
@@ -1,20 +1,26 @@
 module.exports = function (compound) {
 
+    var methodOverride = require('method-override');
+    var bodyParser = require('body-parser');
+    var cookieParser = require('cookie-parser')
+    var session = require('express-session');
+    var multer = require('multer');
+    var logger = require('morgan');
+    var favicon = require('serve-favicon');
     var express = require('express');
     var app = compound.app;
 
-    app.configure(function(){
-        app.use(express.static(app.root + '/public', { maxAge: 86400000 }));
-        app.set('jsDirectory', '/javascripts/');
-        app.set('cssDirectory', '/stylesheets/');
-        app.set('cssEngine', '{{ CSSENGINE }}');
-        compound.loadConfigs(__dirname);
-        app.use(express.urlencoded());
-        app.use(express.json());
-        app.use(express.cookieParser('secret'));
-        app.use(express.session({secret: 'secret'}));
-        app.use(express.methodOverride());
-        app.use(app.router);
-    });
-
+    app.use(express.static(app.root + '/public', { maxAge: 86400000 }));
+    app.set('jsDirectory', '/javascripts/');
+    app.set('cssDirectory', '/stylesheets/');
+    app.set('cssEngine', '{{ CSSENGINE }}');
+    compound.loadConfigs(__dirname);
+    app.use(favicon(app.root + '/public/favicon.ico'));
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(cookieParser('secret'));
+    app.use(session({ resave: true, saveUninitialized: true, secret: 'secret',cookie: { secure: true } }));
+    app.use(methodOverride());
+    app.use(multer());
 };
--- templates/config/environments/production.js
+++ templates/config/environments/production.js
@@ -1,13 +1,16 @@
 var express = require('express');
+var errorHandler = require('errorhandler');
 
 module.exports = function (compound) {
     var app = compound.app;
 
-    app.configure('production', function () {
+    var env = process.env.NODE_ENV || 'production';
+    if ('production' == env) {
+        app.set('defaultLocale', 'en');
         app.enable('quiet');
-        app.enable('merge javascripts');
-        app.enable('merge stylesheets');
+        //app.enable('merge javascripts');
+        // app.enable('merge stylesheets');
         app.disable('assets timestamps');
-        app.use(express.errorHandler());
-    });
+        app.use(errorHandler());
+    }
 };
